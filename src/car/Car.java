b24511c662ebe4060e29cdbe3d55bf723f61490d
Ankit <akankitag@gmail.com>
2023-04-07 11:56:12 +0530

    car service

---
+++ src/car/Car.java
@@ -0,0 +1,16 @@
+package car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Car {
+    public List<String> carList=new ArrayList<>();
+
+    {
+        carList.add("suv");
+        carList.add("sedan");
+        carList.add("xuv");
+        carList.add("creta");
+    }
+
+}
